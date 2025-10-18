beddc6f3817721bb43bd8f06278ee05d3c429a88
MoonLight101 <dmitriy_konovalov@bk.ru>
2025-10-18 16:52:04 +0700

    Add 2 testApps and start work on the 1st

---
+++ practice1/testApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace testApp1;
+					
+public class Program
+{
+	public static void Main(string[] args)
+    {
+        // Прочитать входные данные
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please enter a file name as an argument.");
+            return;
+        }
+        var fileContent = File.ReadAllText(args[0]);
+        Console.WriteLine(fileContent);
+
+        // Отформатировать входные данные 
+
+        // Отобразить отформатированные входные данные
+
+	}
+}
