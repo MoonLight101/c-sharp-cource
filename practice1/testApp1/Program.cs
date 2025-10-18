7b50df574c3ef2b43db6f2af4eea7575b4a618d1
MoonLight101 <dmitriy_konovalov@bk.ru>
2025-10-18 17:13:23 +0700

    Add partial string conversion functionality

--- practice1/testApp1/Program.cs
+++ practice1/testApp1/Program.cs
@@ -10,10 +10,20 @@ public class Program
             Console.WriteLine("Please enter a file name as an argument.");
             return;
         }
-        var fileContent = File.ReadAllText(args[0]);
-        Console.WriteLine(fileContent);
+        string fileContent = File.ReadAllText(args[0]);
 
         // Отформатировать входные данные 
+        string[] coordinates = fileContent.Split('\n');
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            coordinates[i] = coordinates[i].Replace(',', ' ');
+            coordinates[i] = coordinates[i].Replace('.', ',');
+            coordinates[i] = "X: " + coordinates[i];
+        }
+        foreach (var number in coordinates)
+        {
+            Console.WriteLine($"{number}");
+        }
 
         // Отобразить отформатированные входные данные
 
