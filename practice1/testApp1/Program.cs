8358e15384fd50b5d0ce72abad59e701849a4b91
MoonLight101 <dmitriy_konovalov@bk.ru>
2025-10-20 16:26:36 +0700

    Add new implementation inline with task

--- practice1/testApp1/Program.cs
+++ practice1/testApp1/Program.cs
@@ -1,41 +1,31 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+
 namespace testApp1;
 					
 public class Program
 {
-	public static void Main(string[] args)
+	public static void Main()
     {
-        // Прочитать входные данные
-        if (args.Length == 0)
-        {
-            Console.WriteLine("Please enter a txt file name as an argument.");
-            return;
-        }
-        string fileContent = File.ReadAllText(args[0]);
+        // Подготовить переменные для работы программы
+        Dictionary<decimal, decimal> coordinates = new Dictionary<decimal, decimal>();
+        string? buffer;
+        string[] split;
 
-        // Отформатировать входные данные 
-        var result = new Regex("\r\n");
-        fileContent = result.Replace(fileContent, ",");
-
-        string[] numbers = fileContent.Split(',');
-        for (int i = 0; i < numbers.Length; i++)
+        // Прочитать и отформатировать входные данные
+        do
         {
-            numbers[i] = numbers[i].Replace('.', ',');
-        }
-
-        string[] coordinates = new string[numbers.Length / 2];
-        int j = 0;
-        for (int i = 0; i < coordinates.Length; i++)
-        {
-            coordinates[i] = String.Format("Z: {0} Y: {1}", numbers[j], numbers[j + 1]);
-            j += 2;
-        }
+            buffer = Console.ReadLine();
+            if (buffer != null && buffer != "")
+            {
+                split = buffer.Split(',');
+                coordinates.Add(Decimal.Parse(split[0], CultureInfo.InvariantCulture), Decimal.Parse(split[1], CultureInfo.InvariantCulture));
+            }
+        } while (buffer != null && buffer != "");
 
         // Отобразить отформатированные входные данные
-        foreach (var coordinate in coordinates)
+        foreach (KeyValuePair<decimal, decimal> _ in coordinates)
         {
-            Console.WriteLine($"{coordinate}");
+            Console.WriteLine("X: {0} Y: {1}", _.Key, _.Value);
         }
-
 	}
 }
--- practice1/testApp1/obj/Debug/net9.0/testApp1.AssemblyInfo.cs
+++ practice1/testApp1/obj/Debug/net9.0/testApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc02adf4aaca80d69bd58f81124b5f56feebd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48660f7d0610dc27befbfb17c009ab01cf4c9406")]
 [assembly: System.Reflection.AssemblyProductAttribute("testApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
